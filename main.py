dd20c08011caa683094998a95b41ede818cdeba9
MokshaMevada <92812023+MokshaMevada@users.noreply.github.com>
2021-10-20 20:34:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -373,7 +373,6 @@ def makeautomata(f, n):
 if __name__ == '__main__':
     x1 = Int('x1')
     x2 = Int('x2')
-    x3 = Int('x3')
     string = "And(x1 <= 2, x1 + x2 <= 5)"
     arg1 = int(input())
     f = eval(string)
